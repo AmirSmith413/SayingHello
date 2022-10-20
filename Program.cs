6b13a50ef2e79613fa88559c558e5071a9192dc1
AmirSmith413 <amirsmith166@gmail.com>
2022-10-19 20:19:54 -0700

    i think i did it right this time

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿//Mini Challenge #1 
+//Finished on:8/9/22
+//Peer Reviewed by:Saul Suazo
+
+//This program will ask you for your name and say "Hello" to you.
+string name;
+Console.WriteLine("Hello there! What is your name?");
+
+//random name will go here
+name = Console.ReadLine();
+
+//Aw thanks you too <3
+Console.WriteLine($"Hello {name}! I hope you are having a wonderful day!!");         
+
